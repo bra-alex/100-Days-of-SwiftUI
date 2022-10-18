9018400367ac4b2763ec98ba6f85b1c3b6aa7db4
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-10-18 22:20:21 +0000

    Add Tipsy Clone

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var isFocused: Bool
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var total: Double{
+        let tipPercent = Double(tipPercentage)
+        
+        let tip = checkAmount * (tipPercent / 100)
+        let total = checkAmount + tip
+        
+        return total
+    }
+    
+    var totalPerPerson: Double{
+        let persons = Double(numberOfPeople + 2)
+        let tipPercent = Double(tipPercentage)
+        
+        let tip = checkAmount * (tipPercent / 100)
+        let total = checkAmount + tip
+        let amountPerPerson = total / persons
+        
+        return amountPerPerson
+    }
+    
+    var currencyFormat: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "GHS")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "GHS"))
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("Tip Percentage") {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section("Total Amount") {
+                    Text(total, format: currencyFormat)
+                }
+                
+                Section("Amount Per Person") {
+                    Text(totalPerPerson, format: currencyFormat)
+                }
+            }.navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 

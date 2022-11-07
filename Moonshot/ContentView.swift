1275a86ed9f17f1e0966b26f11ac7893e9c5855f
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-11-07 15:28:39 +0000

    Add MissionView and AstronautView

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -11,48 +11,28 @@ struct ContentView: View {
     let astronaut: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let mission: [Mission] = Bundle.main.decode("missions.json")
     
-    let columns = [GridItem(.adaptive(minimum: 150))]
+    @State private var listView = false
     
     var body: some View {
         NavigationStack{
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(mission) { mission in
-                        NavigationLink {
-                            Text("Detail View")
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay{
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            }
-                        }
-                    }
-                }.padding([.horizontal, .bottom])
+            Group{
+                if listView{
+                    ListView(mission: mission, astronaut: astronaut)
+                } else {
+                    GridView(mission: mission, astronaut: astronaut)
+                }
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar{
+                Button {
+                    listView.toggle()
+                } label: {
+                    Image(systemName: listView ? "list.bullet" : "square.grid.2x2")
+                        .animation(.default, value: listView)
+                }
+            }
         }
     }
 }
--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MissionView: View {
     let mission: Mission
+    let astronaut: [String: Astronaut]
     
     var body: some View {
         GeometryReader { proxy in
@@ -20,14 +21,29 @@ struct MissionView: View {
                         .frame(width: proxy.size.width * 0.6)
                         .padding(.top)
                     
+                    Text(mission.formattedLaunchDate)
+                        .padding(.top)
+                        .font(.headline)
+                    
                     VStack(alignment: .leading) {
+                        
+                        DividerView()
+                        
                         Text("Mission Highlights")
                             .font(.title.bold())
                             .padding(.bottom, 5)
                         
                         Text(mission.description)
+                        
+                        DividerView()
+                        
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
                     }
                     .padding(.horizontal)
+                    
+                    CrewView(mission: mission, astronauts: astronaut)
                 }
                 .padding(.bottom)
             }
@@ -36,13 +52,15 @@ struct MissionView: View {
         .navigationBarTitleDisplayMode(.inline)
         .background(.darkBackground)
     }
+    
 }
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronaut: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronaut: astronaut)
             .preferredColorScheme(.dark)
     }
 }

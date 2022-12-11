d68cd6c5672ee876d1b57a6137fd3e5ab8f722f2
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-11 15:24:05 +0000

    Add CoreData

---
+++ Bookworm/Examples/Data.swift
@@ -0,0 +1,40 @@
+//
+//  Data.swift
+//  Bookworm
+//
+//  Created by Don Bouncy on 10/12/2022.
+//
+
+import SwiftUI
+
+struct Data: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+    var body: some View {
+        VStack {
+            List(students) { student in
+                Text(student.name ?? "Unknown")
+            }.listStyle(.plain)
+            
+            Button("Add") {
+                let firstNames = ["Kwabena", "Ciel", "Yaa", "Afia", "Connie"]
+                let lastNames = ["Winston", "Blankson", "Rockson", "Forson", "Dickson"]
+                
+                let firstName = firstNames.randomElement()!
+                let lastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(firstName) \(lastName)"
+                
+                try? moc.save()
+            }
+        }
+    }
+}
+
+struct Data_Previews: PreviewProvider {
+    static var previews: some View {
+        Data()
+    }
+}

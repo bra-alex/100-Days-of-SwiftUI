dfc6f2302b621fe2b6abc45e0622f18cadf70598
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-17 10:40:08 +0000

    Add Filtering.swift

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -5,11 +5,11 @@
 //  Created by Don Bouncy on 17/12/2022.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    
     var body: some View {
         //Check for changes before saving
         /*
@@ -19,24 +19,7 @@ struct ContentView: View {
             }
          }
          */
-        VStack {
-            List(wizards, id:\.self) { wizard in
-                Text(wizard.name ?? "N/A")
-            }
-            
-            Button("Add") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Kyeiwaa"
-            }
-            
-            Button("Save"){
-                do{
-                    try  moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-        }
+        DynamicFiltering()
     }
 }
 

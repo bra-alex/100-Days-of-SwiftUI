f922ff9f62a39abc19bccefbfb37e5e7ecac5022
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2022-12-17 04:55:56 +0000

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Don Bouncy on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

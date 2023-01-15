921d12272fbfec785d928b876d1b1cafbafd0bef
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-01-15 10:55:26 +0000

    Initial Commit

---
+++ Challenge3/Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3App.swift
+//  Challenge3
+//
+//  Created by Don Bouncy on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Challenge3
+//
+//  Created by Don Bouncy on 15/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

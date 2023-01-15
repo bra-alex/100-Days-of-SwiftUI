33bd65230b9327fcfd639870fa41d06a64dfd7f8
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-01-15 11:27:38 +0000

    Load and decode user data from json file

--- Challenge3/ContentView.swift
+++ Challenge3/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List(users) { user in
+                
+                VStack(alignment: .leading){
+                    Text(user.name)
+                    Text(user.isActive ? "Active" : "Inactive")
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            Task{
+                await loadData()
+            }
+        }
+    }
+    
+    func loadData() async {
+
+        if !users.isEmpty{
+            print(users)
+            return
+        }
+        
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Error fetching data")
+            return
+        }
+        
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            if let decodedData = try? decoder.decode([User].self, from: data){
+                users = decodedData
+                print(decodedData)
+            }
+            
+        } catch {
+            print("Could not decode response: \(error.localizedDescription)")
+        }
+        
     }
 }
 

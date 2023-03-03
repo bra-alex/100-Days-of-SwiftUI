6c936d0da41c9020bbc311d7e268d54f86a247cd
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-03 13:35:19 +0000

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Don Bouncy on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

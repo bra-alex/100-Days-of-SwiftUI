f4eacee1d90c7c35a1632181c00a8bbdde06ac54
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-12 09:57:54 +0000

    Initial Commit

---
+++ AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Don Bouncy on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
